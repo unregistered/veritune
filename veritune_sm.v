30ae2ab5ed8429d2ecc2b0fcdb0ade64825f5845
careyzha <careyzha@usc.edu>
2011-04-18 20:00:10 -0700

    Included fft4 sample into state machine code. Some changes to recording.

--- veritune_sm.v
+++ veritune_sm.v
@@ -6,16 +6,40 @@ module veritune_sm(Clk, Reset, Rec, Stop, Play, Freq, Audio_In, q_I, q_Rec, q_St
 	// inputs
 	input Clk, Reset, Rec, Stop, Play;
 	input [7:0] Freq;
-	input Audio_In;
+	input [15:0] Audio_In;
 	
 	// outputs
-	output reg Audio_Out;
+	output reg [15:0] Audio_Out;
 	// store current state
 	output q_I, q_Rec, q_Stop, q_Play;
 	reg [3:0] state;
 	assign {q_Play, q_Stop, q_Rec, q_I} = state;
 	
+	// local
 	localparam I = 4'b0001, REC = 4'b0010, STOP = 4'b0100, PLAY = 4'b1000, UNK = 4'bXXXX;
+	localparam LENGTH = 131070;	// 2^17-2, max number of samples -2
+	reg [15:0] audio_data[16:0];	// unpacked array of N-bits
+	reg [16:0] index;
+	reg [16:0] length;	// actually length recorded -1
+	
+	wire [15:0] fft_in0, fft_in1, fft_in2, fft_in3;
+	wire [31:0] fft_outRe0, fft_outRe1, fft_outRe2, fft_outRe3;
+	wire [31:0] fft_outIm0, fft_outIm1, fft_outIm2, fft_outIm3;
+
+	FFT4 fft4(
+		.X0(fft_in0),
+		.X1(fft_in1),
+		.X2(fft_in2),
+		.X3(fft_in3),
+		.Y0_Re(fft_outRe0),
+		.Y1_Re(fft_outRe1),
+		.Y2_Re(fft_outRe2),
+		.Y3_Re(fft_outRe3),
+		.Y0_Im(fft_outIm0),
+		.Y1_Im(fft_outIm1),
+		.Y2_Im(fft_outIm2),
+		.Y3_Im(fft_outIm3)
+	);
 
 	// NSL and SM
 	always @ (posedge Clk, posedge Reset)
@@ -23,6 +47,7 @@ module veritune_sm(Clk, Reset, Rec, Stop, Play, Freq, Audio_In, q_I, q_Rec, q_St
 		if (Reset)
 		begin : RESET
 			state <= I;
+			index <= 0;
 		end
 		else
 		case (state)
@@ -31,15 +56,22 @@ module veritune_sm(Clk, Reset, Rec, Stop, Play, Freq, Audio_In, q_I, q_Rec, q_St
 				// state transition logic
 				if (Rec)
 					state <= REC;
+				index <= 0;	
 			end
 			REC:
 			begin
 				// state transition logic
-				if (Stop)
+				if (Stop || (index==LENGTH))
+				begin
+					length <= index;
 					state <= STOP;
+				end
 					
 				// clear data first?
+				
 				// store data from audio input
+				audio_data[index] <= Audio_In;
+				index <= index+1;
 			end
 			STOP:
 			begin
@@ -48,15 +80,18 @@ module veritune_sm(Clk, Reset, Rec, Stop, Play, Freq, Audio_In, q_I, q_Rec, q_St
 					state <= PLAY;
 					
 				// just wait? or do frequency shift here?
+				index <= 0;
 			end
 			PLAY:
 			begin
 				// state transition logic
-				if (Stop)
+				if (Stop || index==length)
 					state <= STOP;
 					
 				// do frequency shift?
+				
 				// drive audio output
+
 			end
 			default:
 				state <= UNK;
